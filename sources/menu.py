1f3225a0fafea0ab28f0b2f915f3c3eb01f9b952
rosamartinezru <113106056+rosamartinezru@users.noreply.github.com>
2022-09-18 21:59:46 +0200

    Milestone-1 first approximation

--- sources/menu.py
+++ sources/menu.py
@@ -1,1 +1,21 @@
+#MILESTONE-1
 
+import numpy as np
+
+N = 10000 #Number of partitions
+t_f = 12 * np.pi
+
+t = np.linspace(0,t_f, N) 
+U = np.zeros((4,N+1))
+F = np.zeros(4)
+
+U[:,0] = ([1., 0., 0., 1.]) #Initial conditions
+
+#Explicit Euler Method
+delta_t = t[1]-t[0]
+
+for i in range(0,N):
+    r = U[:-2,i]
+    drdt = U[2:,i]
+    F =  np.concatenate(( drdt, - r / (np.linalg.norm(r)**3)), axis = None) 
+    U[:,i+1] = U[:,i] + delta_t*F

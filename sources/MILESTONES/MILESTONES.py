c4c1ca6c98292668184cbdd063ced6ca11e231bd
rosamartinezru <113106056+rosamartinezru@users.noreply.github.com>
2022-09-24 12:18:41 +0200

    Milestone-1 Updated

--- sources/MILESTONES/MILESTONES.py
+++ sources/MILESTONES/MILESTONES.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 import Functions_Schemes as fs
 
-N = 200
+N = 5000
 
 U_0 = array([1, 0, 0, 1])
 
 t = linspace(0, 50, N)
 
-U = fs.Cauchy_Problem( fs.Kepler_F, t, U_0, fs.Euler)
+U = fs.Cauchy_Problem( fs.Kepler_F, t, U_0, fs.Runge_Kutta_4)
 plt.plot(U[:,0], U[:,1])
-plt.title("Kepler Orbit with dt = 0.25, Euler Temporal Scheme")
+plt.title("Kepler Orbit with dt = 0.01, RK-4 Temporal Scheme")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
